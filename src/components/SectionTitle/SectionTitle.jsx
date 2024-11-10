06ce16e1b168f8f2f924352b3ac4dfd793bc8c4d
subahan ali <subahanislam523@gmail.com>
2024-11-10 20:10:45 +0600

    added new start

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className='md:w-4/12 mx-auto text-center'>
-            <h2 className='text-2xl uppercase '>{heading}</h2>
-            <h5 className='text-orange-600'>---{subHeading}---</h5>
+            <h2 className='text-2xl uppercase mb-2 '>{heading}</h2>
+            <h5 className='text-black font-bold border-y-4 py-4'>---{subHeading}---</h5>
         </div>
     );
 };
--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,7 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-
 import img2 from "../../assets/images/banner/images.jpeg";
 
 const Banner = () => {
