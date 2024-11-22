d787d87134be3604b987dc34effc0e6981b63a50
subahan ali <subahanislam523@gmail.com>
2024-11-22 12:37:50 +0600

    added to by admin panel

--- src/pages/Dashboard/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers.jsx
@@ -2,18 +2,58 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
+import toast from "react-hot-toast";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: user = [] } = useQuery({
+  const { data: user = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch()
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
 
-  const handleDeleteUser = (user) => {};
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your product has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div className="bg-white p-8 rounded-xl shadow-md">
@@ -57,16 +97,20 @@ const AllUsers = () => {
                   {user.email}
                 </td>
                 <td className="px-4 py-4 border-b">
-                  <button
-                    onClick={handleDeleteUser(user)}
-                    className="btn bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-black transition-all"
-                  >
-                    <FaUsers />
-                  </button>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-black transition-all"
+                    >
+                      <FaUsers />
+                    </button>
+                  )}
                 </td>
                 <td className="px-4 py-4 border-b">
                   <button
-                    onClick={handleDeleteUser(user)}
+                    onClick={() => handleDeleteUser(user)}
                     className="btn bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600 transition-all"
                   >
                     <FaTrashAlt />
